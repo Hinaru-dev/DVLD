5b879d64a7cf2aba12c7aa97bd95191271df417f
Hinaru <134238492+Hinaru-dev@users.noreply.github.com>
2025-09-20 08:39:44 +0100

    prepared 3 tiers architecuture layers

---
+++ DVLD Business Logic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business_Logic
+{
+    public class Class1
+    {
+    }
+}
